eb4bb5140d90cdf765812c9adec37eaa78166f31
Henry Urdaneta <urdanetah28@gmail.com>
2025-09-21 23:45:52 -0400

    Current Weather Component and making all JSX

---
+++ src/components/Place.jsx
@@ -0,0 +1,11 @@
+
+function Place() {
+  return (
+    <div className="Place">
+      <i className="bi bi-geo-alt-fill"></i>
+      <b>Lawrenceville</b>, United States
+    </div>
+  )
+}
+
+export default Place
